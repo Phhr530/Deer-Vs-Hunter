1b92cefcc89622b2d1fb1e5e14a2c7f3f520fd9b
Parker robinson <phhr530@outlook.com>
2019-05-08 11:36:52 -0700

    Scope

--- empty-example copy/sketch.js
+++ empty-example copy/sketch.js
@@ -1,9 +1,95 @@
 
+//create a variable to hold your avatar
+let me;
+let r;
+let g;
+let b;
+
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(1000, 780);
+  r=255;
+  g=255;
+  b=255;
+
+  //make one avatar called me
+  me = new Avatar(width/2, 300, 10);
+
 }
 
 function draw(){
-	background(40,255,40);
+	background(r,g,b);
 
+  me.drawMe();
+  me.moveMe();
+
+    if (frameCount % 2 == 0){
+      me.y = me.y + 10
+    }
+
+    if (me.y >= 500){
+      me.y=499
+	  }
+
+}
+
+//avatar class
+class Avatar {
+
+	constructor(x,y, speed){ //every avatar needs an x value, a y value, and a speed
+		    this.x = x;
+    		this.y = y;
+        this.speed = speed;
+	}
+
+
+	drawMe(){
+    		// stroke("green");
+        // strokeWeight(3);
+    		// fill("blue");
+		    // ellipse(this.x,this.y,20,20);
+        // line(this.x,this.y, this.x, this.y+40);
+        // line(this.x, this.y+40, this.x-20, this.y+60);
+        // line(this.x, this.y+40, this.x+10, this.y+50);
+        // line(this.x+10, this.y+50, this.x+5, this.y+60);
+        // line(this.x, this.y+15, this.x-10, this.y+25);
+        // line(this.x-10, this.y+25, this.x+10, this.y+35);
+        ellipse(this.x,this.y,80);
+        line(this.x+10,this.y,this.x+38,this.y)
+        line(this.x-10,this.y,this.x-38,this.y)
+        line(this.x,this.y+10,this.x,this.y+38)
+        line(this.x,this.y-10,this.x,this.y-38)
+          ellipse(this.x,this.y,20);
+	}
+
+	moveMe(){
+    if (keyIsDown(87)) { //if you hold the up arrow, move up by speed
+       this.y -= this.speed;
+    }
+
+    if (keyIsDown(63)){ // if you hold the down arrow, move down by speed
+        this.y += this.speed;
+    }
+    if (keyIsDown(65)){
+      this.x -= this.speed;
+    }
+    if (keyIsDown(68)){
+      this.x += this.speed;
+    }
+    if (keyIsDown(32)){
+      this.y = this.y-d  20
+    }
+	}
+
+  die(){
+if (me.y = 0){
+  me.y = 779
+
+    }
+  }
+ bouncefloor(){
+   if (this.y >= 580){
+   this.jump = -this.jump;
+   }
+
+ }
 }
